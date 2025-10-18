cd7cf3e8cdc1aa6de3edd75ab1711a83f39d47eb
nsns-dev <n.slakaev@onpoint.ru>
2025-10-18 22:55:38 +0300

    update server info in console

--- nodemon, Express/server.js
+++ nodemon, Express/server.js
@@ -6,4 +6,4 @@ const PORT = 1111
 
 app.use(express.json())
 booksRouters.forEach(router => app.use(router))
-app.listen(PORT, () => console.log(`server work on ${PORT}`))
+app.listen(PORT, () => console.log(`[ --- Server launch on ${PORT} port --- ]`))
